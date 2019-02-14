9da6c628209ca5fb7ef46a05a83190d406d6d877
root <root@b7dad7fb72cb.c.t12.run>
2019-02-14 14:30:43 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <img src='http://c93fea60bb98e121740fc38ff31162a8.s3.amazonaws.com/wp-content/uploads/2017/02/lifedojo.jpg' alt="logo" />
+          <h1 className="App-title">Welcome to LifeDojo</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+
+export default App;
