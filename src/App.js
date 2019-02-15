6cc8907a8bb7ab268f7faa29fe4bae86460354ce
Craig Conway <craig.conway@lifedojo.com>
2019-02-15 14:23:10 -0800

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import logo from './logo.jpg'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header>
-          <img src='http://c93fea60bb98e121740fc38ff31162a8.s3.amazonaws.com/wp-content/uploads/2017/02/lifedojo.jpg' alt="logo" />
+          <img src={logo} alt="logo" />
           <h1 className="App-title">Welcome to LifeDojo</h1>
         </header>
       </div>
